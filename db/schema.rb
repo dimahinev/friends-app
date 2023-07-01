4f8fdc368270c8e638f8a03c5bca382525803d44
Dima Hinev <dima.h@index.dev>
2023-07-01 09:57:50 +0300

    database created

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2023_07_01_064501) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
